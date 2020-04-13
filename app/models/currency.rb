dcb46acefe3796a9f9da2fdb13ae90516412dcf0
hughgreethead <hugh.greethead@gmail.com>
2020-04-13 15:45:46 +1000

    Updated routes and models

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,8 @@
 class Currency < ApplicationRecord
+    def current_price
+        # url = 'https://api.coinmarketcap.com/v1/ticker/' - API is no longer running - see learning APIs code - use API sandbox
+        # slugs correspond to the endpoint on the coinmarketcap API so we can get data on each cryptocurrency
+        request = HTTParty.get(url + self.slug)
+        response = JSON.parse(request.body)
+    end
 end

48d2d794d4904aa7d1532193892a917e1ab4674d
HughG-50 <hugh.greethead@gmail.com>
2020-04-18 22:04:07 +1000

    Updated API resource

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,8 +1,14 @@
 class Currency < ApplicationRecord
     def current_price
-        # url = 'https://api.coinmarketcap.com/v1/ticker/' - API is no longer running - see learning APIs code - use API sandbox
+        # Old API format
+        # url = 'https://api.coinmarketcap.com/v1/ticker/' - API is no longer running - use API sandbox to begin with
         # slugs correspond to the endpoint on the coinmarketcap API so we can get data on each cryptocurrency
-        request = HTTParty.get(url + self.slug)
+        # request = HTTParty.get(url + self.slug)
+        # response = JSON.parse(request.body)
+
+        url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        # Need to figure out how to set and retrieve ENV variable for API KEY
+        request = HTTParty.get(url, :query => { :slug => self.slug }, :headers => { 'content-type': 'application/json', 'X-CMC_PRO_API_KEY': API_KEY })
         response = JSON.parse(request.body)
     end
 end

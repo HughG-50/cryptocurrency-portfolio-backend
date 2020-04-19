0705242f0efeb7094a8cf0e4647eb025ee5283eb
HughG-50 <hugh.greethead@gmail.com>
2020-04-19 15:47:48 +1000

    ENV API key storage setup

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-gem 'httparty'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -62,3 +62,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+# **GEMS added to help handle API usage
+# Helps us make GET requests etc to an API from within our code
+gem 'httparty'
+# Helps us handle using JSON
+gem 'json'
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module CryptoPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # This code lets us load ENV variables for our API KEYS from our local_env.yml
+    # file. This overrides ENV variables set in the Unix shell
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

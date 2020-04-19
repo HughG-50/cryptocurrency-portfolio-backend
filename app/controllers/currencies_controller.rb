ad5b92f1cbd44647fbcd8684af31a7a77a85714b
HughG-50 <hugh.greethead@gmail.com>
2020-04-19 17:26:03 +1000

    Model, controller updated

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,10 +4,26 @@ class CurrenciesController < ApplicationController
     end
 
     def search 
-
+        # Comparing the lower case names of what people send through searching for a cryptocurrency to the currencies in our database
+        @currencies = Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
+        # Returning/sending back JSON of all the cryptocurrencies that relate to that users search
+        render json: { currencies: @currencies }
     end
 
+    # Takes in the currency id and the amount owned
+    # Returns final calculations
     def calculate
-        
+        amount = params[:amount]
+        render json: {
+            currency: @currency,
+            current_price: @currency.current_price,
+            amount: amount,
+            value: currency.calculate_value(amount)
+        }
+    end
+
+    private
+    def currency 
+        @currency ||= Currency.find(params[:id])
     end
 end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,14 +1,11 @@
 class Currency < ApplicationRecord
+    def calculate_value(amount)
+        (current_price.to_f * amount.to_f).round(4)
+    end
+    
     def current_price
-        # Old API format
-        # url = 'https://api.coinmarketcap.com/v1/ticker/' - API is no longer running - use API sandbox to begin with
-        # slugs correspond to the endpoint on the coinmarketcap API so we can get data on each cryptocurrency
-        # request = HTTParty.get(url + self.slug)
-        # response = JSON.parse(request.body)
-
         url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-        # Need to figure out how to set and retrieve ENV variable for API KEY
-        #request = HTTParty.get(url, :query => { :slug => self.slug }, :headers => { 'content-type': 'application/json', 'X-CMC_PRO_API_KEY': ENV['CMC_TEST_API_KEY'] })
+        # Can also use IDs or slugs other than the symbol, for the quotes/latest endpoint
         request = HTTParty.get(url, :query => { :symbol => self.currency_symbol }, :headers => { 'content-type': 'application/json', 'X-CMC_PRO_API_KEY': ENV['CMC_TEST_API_KEY'] })
         response = JSON.parse(request.body)['data'][self.currency_symbol]['quote']['USD']['price']
     end

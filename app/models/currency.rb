248ce868e6cccd233badff039e5c5226b1e7992d
HughG-50 <hugh.greethead@gmail.com>
2020-08-13 13:18:56 +1000

    backend controller and model setup

---
+++ app/models/currency.rb
@@ -0,0 +1,13 @@
+class Currency < ApplicationRecord
+    def calculate_value(amount)
+        (current_price.to_f * amount.to_f).round(4)
+    end
+    
+    def current_price
+        # url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+        # Can also use IDs or slugs other than the symbol, for the quotes/latest endpoint
+        request = HTTParty.get(url, :query => { :symbol => self.currency_symbol }, :headers => { 'content-type': 'application/json', 'X-CMC_PRO_API_KEY': ENV['CMC_PRO_API_KEY'] })
+        response = JSON.parse(request.body)['data'][self.currency_symbol]['quote']['USD']['price']
+    end
+end
